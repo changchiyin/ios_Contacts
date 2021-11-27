c281e21c85cf4dbaf75923a3b19ba41441cdacc0
dejes <jack940197@gmail.com>
2021-11-27 11:54:13 +0800

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by User10 on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
